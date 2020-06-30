4fae0444bf6852f3577dedb55033961b74d2370d
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-30 11:48:26 -0700

    add functionality to buttons and dynamically show tamagotchi properties level
    co-authored-by: Kate Skorija <kateskorija@gmail.com>
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,48 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from 'jquery';
+import { Tamagotchi } from './tamagotchi';
+import $ from 'jquery';
+
+$(document).ready(function(){
+  let tamagotchi = new Tamagotchi();
+  $(".hungerLevel").text(tamagotchi.hunger);
+  $(".sleepLevel").text(tamagotchi.sleep);
+  $(".happinessLevel").text(tamagotchi.happiness);
+  $(".cleanlinessLevel").text(tamagotchi.cleanliness);
+  $(".healthLevel").text(tamagotchi.health);
+  $(".sickStatus").text(tamagotchi.isSick ? "yes" : "no");
+  tamagotchi.setHunger();
+  tamagotchi.setSleep();
+  tamagotchi.setHappiness();
+  tamagotchi.setCleanliness();
+  tamagotchi.setHealth();
+  tamagotchi.setIsSick();
+  setInterval(() => {
+    $(".hungerLevel").text(tamagotchi.hunger);
+    $(".sleepLevel").text(tamagotchi.sleep);
+    $(".happinessLevel").text(tamagotchi.happiness);
+    $(".cleanlinessLevel").text(tamagotchi.cleanliness);
+    $(".healthLevel").text(tamagotchi.health);
+    $(".sickStatus").text(tamagotchi.isSick ? "yes" : "no");
+  }, 1000);
+  $(".feed").click(function() {
+    tamagotchi.feed();
+    $(".hungerLevel").text(tamagotchi.hunger);
+  });
+  
+  $(".putToBed").click(function() {
+    tamagotchi.putToBed();
+    $(".sleepLevel").text(tamagotchi.sleep);
+  });
+  
+  $(".play").click(function() {
+    tamagotchi.play();
+    $(".happinessLevel").text(tamagotchi.happiness);
+  });
+  
+  $(".wash").click(function() {
+    tamagotchi.wash();
+    $(".cleanlinessLevel").text(tamagotchi.cleanliness);
+  });
+});
